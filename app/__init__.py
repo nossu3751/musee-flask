90d225e9e0ed111d0e4362ac38e56d556b26a3de
Rhosung Park <rhosungpark@gmail.com>
2023-08-18 01:41:45 -0700

    installed psycopg2, added example view

--- app/__init__.py
+++ app/__init__.py
@@ -4,21 +4,18 @@ from flask_cors import CORS
 from .extensions import redis_wrapper, db
 from app.api.demo.views import demo_blueprint
 from dotenv import load_dotenv
+from config import Config
 
 origins = ["*"]
 
 def create_app():
     load_dotenv()
     app = Flask(__name__)
+    app.config.from_object(Config)
     CORS(app, origins=origins, supports_credentials=True)
 
     app.register_blueprint(demo_blueprint)
 
-    redis_wrapper.init(
-        host=os.getenv('REDIS_HOST'),
-        port=os.getenv('REDIS_PORT'),
-        decode_responses=True
-    )
     db.init_app(app)
 
     return app
--- app/api/demo/views.py
+++ app/api/demo/views.py
@@ -4,6 +4,10 @@ from app.api.demo.utils import *
 
 demo_blueprint = Blueprint('demo', __name__, url_prefix='/api/demo')
 
+@demo_blueprint.route("/", methods=["GET"])
+def hello_world():
+    return jsonify("hello")
+
 @demo_blueprint.route("/users/", methods=["GET"])
 def get_users():
-    ...
+    return jsonify("users")
