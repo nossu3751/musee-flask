3e098387664eccee982272278a7048a45ce07823
Young Suh <yahoyoungho@gmail.com>
2023-08-26 21:23:53 +0000

    editing verif vote data creation logic

--- app/models/demo/artwork.py
+++ app/models/demo/artwork.py
@@ -14,7 +14,9 @@ class Artwork(db.Model):
     actual_price = db.Column(db.Integer, nullable=True)
     name = db.Column(db.String, nullable=False) 
     votes = relationship("VerifVote", backref="artwork", cascade="all,delete")
-    img_link = db.Column(db.String, nullable=False) 
+    img_link = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=True)
+    awSize = db.Column(db.String, nullable=True)
    
     def __repr__(self):
         return f'<Artwork {self.id}>'
